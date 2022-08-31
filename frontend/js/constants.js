440ccba2deeb0d8234aad8fe394de06c33367495
MJundi2022 <mohamad.jundi@hotmail.com>
2022-08-31 18:16:53 +0300

    test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the RYLA NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Welcome to the RYLA NFT Project!!';
+const welcome_p = "Welcome to the RYLA NFT Project!!";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
