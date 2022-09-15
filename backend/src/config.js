de8c81a6ea52ac87b3c83c45595c900619d57690
MJundi2022 <mohamad.jundi@hotmail.com>
2022-09-15 17:38:30 +0300

    now

--- backend/src/config.js
+++ backend/src/config.js
@@ -72,7 +72,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0x53dd822c10351620D728A205172313f1e5b03FaC"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x8A97D6A37e70E05BfeAC024F1CA96ba3F6Fa171D"; // If you want to manually include it
+let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
