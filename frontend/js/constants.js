6c6b2de68bdcbe2c4211bb3d0d76eef5718163f8
MJundi2022 <mohamad.jundi@hotmail.com>
2022-09-27 19:46:16 +0300

    newcontract

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x8a97d6a37e70e05bfeac024f1ca96ba3f6fa171d"; // Replace with your own contract address
+const contractAddress = "0x32D9a4C4BAF994E1798D6b6f660deecc353F5E7c"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the RYLA NFT Project!!";
